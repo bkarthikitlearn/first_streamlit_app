95ee381c0a9b15f70f1041c59a8623f731418fd1
Karthik Baskaran <bkarthikitlearn@gmail.com>
2022-08-28 19:39:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# reads fruityvice json data with pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# creates dataframe and displays as table of fruityvice data
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    # reads fruityvice json data with pandas
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # creates dataframe and displays as table of fruityvice data
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 #don't run anything after here
 streamlit.stop()
 
